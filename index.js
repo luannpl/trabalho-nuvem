78c39ba8208f7e48ea4cf9447bfca3d3c3d2edc2
Paulo Luan <pauloluan1409@gmail.com>
2024-04-24 16:19:33 -0300

    Adicionando arquivo index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/", function(req,res){
+    res.send("Bem vindo");
+})
+
+app.get("/login", function(req,res){
+    res.send("<h1>Faça seu login!</h1>");
+})
+
+app.get("/login/suporte", function(req,res){
+    res.send("<h1>Está precisando de ajuda?</h1> <button>Sim</button> <button>Não</button>");
+})
+
+
+app.listen(4000, function(erro){
+    if(erro){
+        console.log("Erro ao iniciar.");
+    }else{
+        console.log("Servidor Iniciado")
+    }
+})
